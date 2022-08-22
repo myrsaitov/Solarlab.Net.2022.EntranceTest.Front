54bda4e86525bceecbc8a06e71f254f4f6978775
Konstantin Myrsaitov <myrsaitov@gmail.com>
2022-08-23 02:30:59 +0300

    Верстка в первом приближении готова. Теперь нужны сервисы

--- src/app/app-components/ui-elements/right-column/right-column.component.ts
+++ src/app/app-components/ui-elements/right-column/right-column.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectorRef, Component, HostListener, OnInit, ViewChild} from '@angular/core';
 import {tags} from "../../../interfaces/tag/itag";
+import {VirtualScrollerComponent} from "ngx-virtual-scroller";
 
 @Component({
   selector: 'app-right-column',
@@ -7,10 +8,47 @@ import {tags} from "../../../interfaces/tag/itag";
   styleUrls: ['./right-column.component.css']
 })
 export class RightColumnComponent implements OnInit {
-  tags = [...tags];
-  constructor() { }
 
-  ngOnInit(): void {
+  // Относительные размеры области Virtual Scroller
+  k_height = 0.8;
+  k_width = 0.2;
+
+  // Хранит размеры области Virtual Scroller
+  viewVirtualScrollerAreaHeight: any;
+  viewVirtualScrollerAreaWidth: any;
+
+  @ViewChild('scroll') scroller: VirtualScrollerComponent;
+
+  tags = [...tags];
+  constructor(private cd: ChangeDetectorRef) { }
+
+  ngOnInit(){
+    // Обновляет размеры карточки в соответсвии с размером экрана
+    this.viewVirtualScrollerAreaHeight = window.innerHeight*this.k_height;
+    this.viewVirtualScrollerAreaWidth = window.innerWidth*this.k_width;
+  }
+
+  // Обработка события изменения размера главного окна
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.viewVirtualScrollerAreaHeight = event.target.innerHeight*this.k_height;
+    this.viewVirtualScrollerAreaWidth = event.target.innerWidth*this.k_width;
+  }
+
+  onVsUpdate(event) {
+    console.log('vs update', event);
+  }
+
+  onVsChange(event) {
+    console.log('vs change', event);
+  }
+
+  onVsStart(event) {
+    console.log('vs start', event);
+  }
+
+  onVsEnd(event) {
+    console.log('vs end', event);
   }
 
 }
